bb10c1211d934d99cac0ec599621a5136799f3e9
santrix12G <craklola12@gmail.com>
2025-10-14 17:46:41 -0500

    The simpsons api super

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ApiSimpsons_2025IIg2_Cuellar_Santiago/'
+  base: '/ApiSimpsons_2025IIg2_Carlos_Santiago/'
 })
